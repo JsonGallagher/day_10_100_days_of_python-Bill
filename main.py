0651c56aa7d95170a5338cf5fa5972e650e00359
Jason Gallagher <jasong13@gmail.com>
2022-11-17 15:49:44 +0000

    Added a tip calculator

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-myBill = float(input("What was the bill?: "))
-numberOfPeople = int(input("How many people?: "))
-answer = myBill / numberOfPeople
-bill_displayed_in_currency = "{:.2f}".format(answer)
+bill = float(input("What was the bill: "))
+tip_percentage = int(input("What tip percentage would you like to add? "))
+bill_total = bill + (bill * (tip_percentage/100))
+bill_total_formatted = "{:.2f}".format(bill_total)
+print(f"The total bill is: ${bill_total_formatted}")
+print()
+num_people = int(input("How many people will be splitting the bill? "))
+bill_per_person = bill_total / num_people
+bill_per_person_formatted = "{:.2f}".format(bill_per_person)
 
-print(f"You all owe ${bill_displayed_in_currency}")
+print(f"You all owe ${bill_per_person_formatted}")
